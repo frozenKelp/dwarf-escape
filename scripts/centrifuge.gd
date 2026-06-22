39278eee2a332718563cb579d26c3fd40af7389c
frozenKelp <anubhab1810@gmail.com>
2026-06-22 12:07:25 +0530

    initial commit

---
+++ scripts/centrifuge.gd
@@ -0,0 +1,56 @@
+extends Node2D
+@onready var sprite: Sprite2D = $Sprite
+
+const allow_radius = 200
+
+enum STATE { EMPTY, SPIN_UP, HOLD, UNSTABLE } #constants
+var state := STATE.EMPTY #CENTRIFUGE STATE
+# target band:
+var target_min := 30.0
+var target_max := 45.0
+
+# gets mouse angular velocity
+var last_mouse_position := get_local_mouse_position()
+func mAngVel(mouse_position, time):
+	var deltaAngle = last_mouse_position.angle_to(mouse_position)
+	last_mouse_position = mouse_position
+	var mouse_angular_velocity = deltaAngle/time
+	print("mouse ang.velocity in rad/sec: ", snapped(mouse_angular_velocity, 0.01)) # snapped is approx
+	
+	if mouse_position.length() < allow_radius:
+		last_mouse_position = mouse_position   # avoids a spike on re-entry
+		return 0.0 
+	return mouse_angular_velocity # in rad/sec
+	
+
+# rotates sprite
+var aa = 3
+var angular_accleration = 3 # radians/sec^2
+var angular_velocity = 0 # initial angular vel
+func sprite_rotate(mav, time):
+	#use higher accleration when stoping for friction feel?
+	if abs(angular_velocity) > abs(mav):
+		angular_accleration = 2*aa
+	else:	
+		angular_accleration = aa
+	print(angular_accleration)
+	angular_velocity = move_toward(angular_velocity, mav, angular_accleration * time)
+	sprite.rotation += angular_velocity*time
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+#region set spin state
+	var s = abs(angular_velocity)
+	if s < target_min:
+		state = STATE.SPIN_UP   # "spin faster"
+	elif s <= target_max:
+		state = STATE.HOLD      # the sweet spot
+	else:
+		state = STATE.UNSTABLE  # too fast
+#endregion
+	
+	var mouse_pos = get_local_mouse_position()
+	var mouse_angular_velocity = mAngVel(mouse_pos, delta)
+	sprite_rotate(mouse_angular_velocity, delta)
---
+++ scripts/centrifuge_sprite.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _process(delta: float) -> void:
+	print("sprite velocity : ", say_vel(delta))
+	
+var oldangle = self.rotation
+func say_vel(time):
+	var angle = self.rotation
+	var angledelta =  angle - oldangle
+	var velocity = snapped(angledelta/time, 0.01)
+	
+	oldangle = angle
+	return velocity

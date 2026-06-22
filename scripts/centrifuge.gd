e21b6237164c299d95cf64673d0208dd24e434f5
frozenKelp <anubhab1810@gmail.com>
2026-06-22 15:15:27 +0530

    added gamestate, updated readme, added instablity ad progress

--- scripts/centrifuge.gd
+++ scripts/centrifuge.gd
@@ -6,10 +6,10 @@ const allow_radius = 100
 enum STATE { EMPTY, SPIN_UP, HOLD, UNSTABLE } #constants
 var state := STATE.EMPTY #CENTRIFUGE STATE
 # target band:
-var target_min := 30.0
-var target_max := 45.0
-var progress := 0
-var instability := 0
+var target_min := 12.0
+var target_max := 18.0
+var progress := 0.00
+var instability := 0.00
 
 # gets mouse angular velocity
 var last_mouse_position := get_local_mouse_position()
@@ -41,11 +41,6 @@ func sprite_rotate(mav, time):
 	sprite.rotation += angular_velocity*time
 
 
-
-
-
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 #region set spin state
@@ -58,6 +53,13 @@ func _process(delta: float) -> void:
 		state = STATE.UNSTABLE  # too fast
 #endregion
 	
+	if state == STATE.HOLD:
+		progress += delta
+	elif state == STATE.UNSTABLE:
+		instability += delta
+	
+	
 	var mouse_pos = get_local_mouse_position()
 	var mouse_angular_velocity = mAngVel(mouse_pos, delta)
 	sprite_rotate(mouse_angular_velocity, delta)
+	print(progress,instability)
